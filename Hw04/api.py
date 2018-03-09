766ab3e21628a743cc9d9c2b8f9ee72f06faefb2
thanapoomo58 <35727362+thanapoomo58@users.noreply.github.com>
2018-03-09 22:53:04 +0700

    Update api.py

--- Hw04/api.py
+++ Hw04/api.py
@@ -4,28 +4,21 @@ from flask_restful import Resource ,Api ,reqparse
 app= Flask(__name__)
 api = Api(app)
 
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
-
 parser = reqparse.RequestParser()
-parser.add_argument('studentcode')
+parser.add_argument('studentid')
 
 def convertToBinary(n):
    return bin(n)[2:]
-class studentcodeToBinary(Resource):
+class studentidToBinary(Resource):
         def post(self):
                 args = parser.parse_args()
-                studentcode = args['studentcode']
-                studentcode_d = int(studentcode)
-                studentcode_b = convertToBinary(studentcode_d)
+                studentid = args['studentid']
+                studentid_d = int(studentid)
+                studentid_b = convertToBinary(studentid_d)
                 
-                return {"studentcode":studentcode_b}
+                return {"studentid":studentid_b}
 
-api.add_resource(studentcodeToBinary,'/api/studentcode-to-binary')
+api.add_resource(studentidToBinary,'/api/studentid_to_binary')
 
 
 if __name__ == '__main__':
